a0cd75fda6da8f4a1fd69b46c59885e0a083b95d
singhsamarp001-commits <singhsamarp001@gmail.com>
2026-09-09 01:29:52 +0530

    Time: 4 ms (100%), Space: 45.7 MB (86.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0 || (x%10==0 && x!=0)){
+            return false;
+        }
+        int reversedNumber = 0;
+        while (x > reversedNumber){
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x = x/10;
+        }
+        //x==reversedNumber;
+        //x==reversedNumber/10;
+        return x == reversedNumber || x == reversedNumber/10;
+        
+    }
+}
