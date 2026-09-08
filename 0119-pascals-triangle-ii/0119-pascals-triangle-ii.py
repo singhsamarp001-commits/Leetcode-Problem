52679dad30c9e3fb95b4a696ce7f862b3a9c2546
singhsamarp001-commits <singhsamarp001@gmail.com>
2026-09-09 01:35:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.12%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        row = [1]
+        for i in range(rowIndex):
+            new_row = [1]
+            for j in range(1, len(row)):
+                new_row.append(row[j - 1] + row[j])
+            new_row.append(1)
+            row = new_row
+        return row
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        
