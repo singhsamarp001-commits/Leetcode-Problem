b7c0586d9f9af98b04a033c3303d93834107a5ed
singhsamarp001-commits <singhsamarp001@gmail.com>
2026-09-15 23:04:52 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.54%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+        """
+        :type n: int
+        :rtype: int
+        """
+        
